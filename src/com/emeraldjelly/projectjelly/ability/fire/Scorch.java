882e1f29ef022390d27db96e530a1ee26cdade74
EmeraldJelly <nickb122717@gmail.com>
2017-10-07 14:58:30 -0400

    Scorch Desc

--- src/com/emeraldjelly/projectjelly/ability/fire/Scorch.java
+++ src/com/emeraldjelly/projectjelly/ability/fire/Scorch.java
@@ -36,7 +36,12 @@ public class Scorch extends FireAbility implements AddonAbility {
 	}
 	
 	public String getDescription() {
-		JellyMethods.fireDesc("Offensive/Utility", "");
+		return JellyMethods.fireDesc("Offensive/Utility", "A charge up advanced firebending move. This move allows the firebender to charge up to 3 different power levels.\r\n" + 
+				"\r\n" + 
+				"Power Level 1: The Weakest charge level. Can only deal a average fireblast to an enemy. (Just a simple Click doesnt require any charging by shifting)\r\n" + 
+				"Power Level 2: A strong charge level, however not the strongest. This allows the fire bender to create a small but concentrated blast that does pretty significant damage. (Requires a 5 Second Charge)\r\n" + 
+				"\r\n" + 
+				"Power Level 3: A overwhelmingly powerful charge level. This does not enable the firebender to fire a \"Blast\" however this does make it so the fire bender can move at incredibly fast moving speeds. While in this move the firebender damages Everything within a 3 block radius of them. The firebender may also FLY (using thrust from fire) during this mode but be warned, not only does this take 10 seconds to charge but it also puts a huge strain on the firebender not allowing them to bend for 10 seconds after using this ability.");
 	}
 
 	@Override
