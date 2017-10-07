d64f07d1819950c72e9d250817c78da7afe8de44
EmeraldJelly <nickb122717@gmail.com>
2017-10-07 14:42:49 -0400

    JellyMethod Desc.

--- src/com/emeraldjelly/projectjelly/utility/JellyMethods.java
+++ src/com/emeraldjelly/projectjelly/utility/JellyMethods.java
@@ -8,7 +8,7 @@ public class JellyMethods {
 	 * This class is to be used to store any useful methods.
 	 * These can be called at any time by any class, ability ect ect..
 	 * You May add anything to this Class if you think it will be useful. 
-	 * Please make sure not to break this class.
+	 * Please make sure not to break this class. As All abilities will use this class.
 	 */
 	
 	public static boolean isSpigot() {
