d529fe8fbb974f62788c47a1230b768f41d28659
EmeraldJelly <nickb122717@gmail.com>
2017-10-07 15:07:24 -0400

    Another Syntax Fix.

--- src/com/emeraldjelly/projectjelly/ability/water/Sprout.java
+++ src/com/emeraldjelly/projectjelly/ability/water/Sprout.java
@@ -133,7 +133,7 @@ public class Sprout extends PlantAbility implements AddonAbility {
 	
 	@Override
 	public String getDescription() {
-		return JellyMethods.waterDesc()"Utility", "This ability allows skilled plantbenders to bring up the roots underground and shoot them at their opponent. This moves requires that you be standing on fertile ground (grass blocks). ");
+		return JellyMethods.waterDesc("Utility", "This ability allows skilled plantbenders to bring up the roots underground and shoot them at their opponent. This moves requires that you be standing on fertile ground (grass blocks). ");
 	}
 	
 	@Override
