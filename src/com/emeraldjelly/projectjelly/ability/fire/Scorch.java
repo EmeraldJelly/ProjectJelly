86aafdc23a6cc788737add9ad1712469d01fe5e1
EmeraldJelly <nickb122717@gmail.com>
2017-10-08 11:36:37 -0400

    Progress on Scorch. Still dont work right.

--- src/com/emeraldjelly/projectjelly/ability/fire/Scorch.java
+++ src/com/emeraldjelly/projectjelly/ability/fire/Scorch.java
@@ -51,9 +51,12 @@ public class Scorch extends FireAbility implements AddonAbility {
 		this.d3 = ConfigManager.getConfig().getInt(JellyMethods.configPath("fire", "Scorch", "Damage3"));
 		this.r1 = ConfigManager.getConfig().getInt(JellyMethods.configPath("fire", "Scorch", "Range1"));
 		this.r2 = ConfigManager.getConfig().getInt(JellyMethods.configPath("fire", "Scorch", "Range2"));
-		this.chT1 = ConfigManager.getConfig().getInt(JellyMethods.configPath("fire", "Scorch", "PowerLevel2ChargeTime"));
-		this.chT2 = ConfigManager.getConfig().getInt(JellyMethods.configPath("fire", "Scorch", "PowerLevel3ChargeTime"));
-		this.duration = ConfigManager.getConfig().getInt(JellyMethods.configPath("fire", "Scorch", "DurationInSeconds"));
+		this.chT1 = ConfigManager.getConfig()
+				.getInt(JellyMethods.configPath("fire", "Scorch", "PowerLevel2ChargeTime"));
+		this.chT2 = ConfigManager.getConfig()
+				.getInt(JellyMethods.configPath("fire", "Scorch", "PowerLevel3ChargeTime"));
+		this.duration = ConfigManager.getConfig()
+				.getInt(JellyMethods.configPath("fire", "Scorch", "DurationInSeconds"));
 		this.loc = player.getLocation();
 		ch1 = false;
 		ch2 = false;
@@ -76,17 +79,18 @@ public class Scorch extends FireAbility implements AddonAbility {
 	}
 
 	public String getDescription() {
-		return JellyMethods.abilityDescription(false, "Offensive/Utility", "fire", "A charge up advanced firebending move. This move allows the firebender to charge up to 3 different power levels.\r\n"
-				+ "\r\n"
-				+ "Power Level 1: The Weakest charge level. Can only deal a average fireblast to an enemy.\r\n"
-				+ "Power Level 2: A strong charge level, however not the strongest. This allows the fire bender"
-				+ " to create a small but concentrated blast that does pretty significant damage. (Requires a 5 Second Charge)\r\n"
-				+ "\r\n"
-				+ "Power Level 3: A overwhelmingly powerful charge level. This does not enable the firebender to fire a "
-				+ "\"Blast\" however this does make it so the fire bender can move at incredibly fast moving speeds. "
-				+ "While in this move the firebender damages Everything within a 3 block radius of them. The firebender"
-				+ " may also FLY (using thrust from fire) during this mode but be warned, it also puts a huge strain on "
-				+ "the firebender not allowing them to bend for 10 seconds after using this ability.");
+		return JellyMethods.abilityDescription(false, "Offensive/Utility", "fire",
+				"A charge up advanced firebending move. This move allows the firebender to charge up to 3 different power levels.\r\n"
+						+ "\r\n"
+						+ "Power Level 1: The Weakest charge level. Can only deal a average fireblast to an enemy.\r\n"
+						+ "Power Level 2: A strong charge level, however not the strongest. This allows the fire bender"
+						+ " to create a small but concentrated blast that does pretty significant damage. (Requires a 5 Second Charge)\r\n"
+						+ "\r\n"
+						+ "Power Level 3: A overwhelmingly powerful charge level. This does not enable the firebender to fire a "
+						+ "\"Blast\" however this does make it so the fire bender can move at incredibly fast moving speeds. "
+						+ "While in this move the firebender damages Everything within a 3 block radius of them. The firebender"
+						+ " may also FLY (using thrust from fire) during this mode but be warned, it also puts a huge strain on "
+						+ "the firebender not allowing them to bend for 10 seconds after using this ability.");
 	}
 
 	public String getInstructions() {
@@ -105,7 +109,7 @@ public class Scorch extends FireAbility implements AddonAbility {
 
 	@Override
 	public void progress() {
-		if (bPlayer.canBend(this)) {
+		if (!bPlayer.canBend(this)) {
 			remove();
 			return;
 		}
@@ -113,83 +117,58 @@ public class Scorch extends FireAbility implements AddonAbility {
 			if (System.currentTimeMillis() - getStartTime() > chT1) {
 				ch1 = true;
 				chargeRingOne(40, 0.75F, 2);
-				if (ch1) {
-					if (!player.isSneaking()) {
-						loc.add(player.getLocation().getDirection().multiply(1));
-						ParticleEffect.FLAME.display(loc, 0.6F, 0.6F, 0.6F, 0, 40);
-						for (Entity entity : GeneralMethods.getEntitiesAroundPoint(player.getLocation(), 3)) {
-							if (entity instanceof LivingEntity && entity.getEntityId() != player.getEntityId()) {
-								DamageHandler.damageEntity(entity, d2, this);
-							}
-						}
-						if (GeneralMethods.isSolid(loc.getBlock())) {
-							remove();
-							this.cooldown = cooldown2;
-							bPlayer.addCooldown(this);
-							return;
-						}
-						if (player.getLocation().distanceSquared(loc) > r2) {
-							remove();
-							this.cooldown = cooldown2;
-							bPlayer.addCooldown(this);
-							return;
-						}
-						remove();
-						this.cooldown = cooldown2;
-						bPlayer.addCooldown(this);
-						return;
-					}
-				}
-				if (System.currentTimeMillis() - getStartTime() > chT2) {
-					ch1 = false;
-					ch2 = true;
-					chargeRingTwo(40, 1F, 2);
-					chargeRingOne(40, 1.75F, 2);
-					if (ch2) {
-						if (!player.isSneaking()) {
-							player.setAllowFlight(true);
-							player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, duration * 100, 3));
-							for (Entity entity : GeneralMethods.getEntitiesAroundPoint(player.getLocation(), 3)) {
-								if (entity instanceof LivingEntity && entity.getEntityId() != player.getEntityId()) {
-									DamageHandler.damageEntity(entity, d3, this);
-									entity.setVelocity(new Vector(0.0D, 0.5D, 0.0D).add(GeneralMethods
-											.getDirection(player.getLocation(), entity.getLocation()).multiply(2)));
-								}
-							}
-							if (System.currentTimeMillis() - getStartTime() > duration) {
-								remove();
-								this.cooldown = cooldown3;
-								player.setAllowFlight(false);
-								bPlayer.addCooldown(this);
-								return;
-							}
-						}
-					}
-				} else if (!player.isSneaking()) {
-					loc.add(player.getLocation().getDirection().multiply(1));
-					ParticleEffect.FLAME.display(loc, 0.5F, 0.5F, 0.5F, 0, 20);
-					for (Entity entity : GeneralMethods.getEntitiesAroundPoint(player.getLocation(), 3)) {
-						if (entity instanceof LivingEntity && entity.getEntityId() != player.getEntityId()) {
-							DamageHandler.damageEntity(entity, d1, this);
-						}
-					}
-					if (GeneralMethods.isSolid(loc.getBlock())) {
-						remove();
-						this.cooldown = cooldown2;
-						bPlayer.addCooldown(this);
-						return;
-					}
-					if (player.getLocation().distanceSquared(loc) > r1) {
-						remove();
-						this.cooldown = cooldown1;
-						bPlayer.addCooldown(this);
-						return;
-					}
+				if (GeneralMethods.isSolid(loc.getBlock())) {
 					remove();
+					player.sendMessage("Removed Due to IsSolid Issues");
 					this.cooldown = cooldown2;
 					bPlayer.addCooldown(this);
 					return;
 				}
+				if (player.getLocation().distanceSquared(loc) > r2) {
+					remove();
+					this.cooldown = cooldown2;
+					player.sendMessage("Removed Due to Range Issues");
+					bPlayer.addCooldown(this);
+					return;
+				}
+			}
+			if (ch1) {
+				if (!player.isSneaking()) {
+					loc.add(player.getLocation().getDirection().multiply(1));
+					ParticleEffect.FLAME.display(loc, 0.6F, 0.6F, 0.6F, 0, 40);
+					for (Entity entity : GeneralMethods.getEntitiesAroundPoint(player.getLocation(), 3)) {
+						if (entity instanceof LivingEntity && entity.getEntityId() != player.getEntityId()) {
+							DamageHandler.damageEntity(entity, d2, this);
+						}
+					}
+				}
+			}
+			if (System.currentTimeMillis() - getStartTime() > chT2) {
+				ch1 = false;
+				ch2 = true;
+				chargeRingTwo(40, 1F, 2);
+				chargeRingOne(40, 1.75F, 2);
+			}
+			if (ch2) {
+					player.sendMessage("It Was Ran");
+					player.setAllowFlight(true);
+					player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, duration * 100, 3));
+					if (!player.isSneaking()) {
+						for (Entity entity : GeneralMethods.getEntitiesAroundPoint(player.getLocation(), 3)) {
+							if (entity instanceof LivingEntity && entity.getEntityId() != player.getEntityId()) {
+								DamageHandler.damageEntity(entity, d3, this);
+								entity.setVelocity(new Vector(0.0D, 0.5D, 0.0D).add(GeneralMethods
+										.getDirection(player.getLocation(), entity.getLocation()).multiply(2)));
+								long startTime = System.currentTimeMillis();
+								if (System.currentTimeMillis() - startTime > duration) {
+									remove();
+									player.sendMessage("DURATION");
+									bPlayer.addCooldown(this);
+									return;
+								}
+							}
+						}
+					}
 			}
 		}
 	}
